82419de05d433aa92ce491b7d8433f34434922f2
Costin Lupu <costin.lup@gmail.com>
2023-01-29 12:16:11 +0000

    cloning: Config.mk: Use cloning repo for Qemu 03
    Signed-off-by: Costin Lupu <costin.lup@gmail.com>

--- Config.mk
+++ Config.mk
@@ -239,7 +239,7 @@ SEABIOS_UPSTREAM_URL ?= http://xenbits.xen.org/git-http/seabios.git
 MINIOS_UPSTREAM_URL ?= http://xenbits.xen.org/git-http/mini-os.git
 else
 OVMF_UPSTREAM_URL ?= git://xenbits.xen.org/ovmf.git
-QEMU_UPSTREAM_URL ?= git://github.com:nephele-vm/qemu-xen.git
+QEMU_UPSTREAM_URL ?= git@github.com:nephele-vm/qemu-xen.git
 QEMU_TRADITIONAL_URL ?= git://xenbits.xen.org/qemu-xen-traditional.git
 SEABIOS_UPSTREAM_URL ?= git://xenbits.xen.org/seabios.git
 MINIOS_UPSTREAM_URL ?= git://xenbits.xen.org/mini-os.git
